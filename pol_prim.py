bbf43760d0aceb50d950d648b2837caaa7b1e99d
pdm <pdm@korjkx.ru>
2022-10-12 12:16:59 +0300

    01

--- pol_prim.py
+++ pol_prim.py
@@ -36,7 +36,9 @@ class Glavni_prozor(QWidget):
     def __init__(self):
         super(Glavni_prozor, self).__init__()
         self.setFixedSize(1024, 768)
+
         self.edit_window = edit_window(self)
+
         layout = QVBoxLayout(self)
 
         self.tabelaEditovanje = QTableView()
@@ -47,14 +49,38 @@ class Glavni_prozor(QWidget):
         self.tabelaEditovanje.doubleClicked.connect(self.prikazi_drugi)
 
         layout.addWidget(self.tabelaEditovanje)
+
         # pravi promjenjivu za pretragu baze podataka
         self.model = QtSql.QSqlQueryModel()
-        self.model.setQuery("SELECT Lokacija,Kancelarija,Prezime,Ime,Telefon,Lokal,Fax,Oblast FROM telImenik")
+
+        ## ---
+        ##self.model.setQuery("SELECT Lokacija,Kancelarija,Prezime,Ime,Telefon,Lokal,Fax,Oblast FROM telImenik")
+        # +++ id, !!!
+        self.model.setQuery("""SELECT 
+                                   id,                
+                                   Lokacija,
+                                   Kancelarija,
+                                   Prezime,
+                                   Ime,
+                                   Telefon,
+                                   Lokal,
+                                   Fax,
+                                   Oblast 
+                               FROM telImenik""")
+
         # upisuje rezultate pretrage baze u self.tableView
         self.tabelaEditovanje.setModel(self.model)
-        # odredjuje sirinu kolona u self.tableView-u prva kolona je 0-nulta,id nema potrebe dodavati jer ga sam odredjuje
-        for i, width in enumerate([150, 65, 100, 80, 90, 40, 80, 340]):
+
+        # ---
+        ## odredjuje sirinu kolona u self.tableView-u prva kolona je 0-nulta,id nema potrebe dodavati jer ga sam odredjuje
+        ##for i, width in enumerate([150, 65, 100, 80, 90, 40, 80, 340]):
+        ##    self.tabelaEditovanje.setColumnWidth(i, width)
+
+        # +++
+        for i, width in enumerate([10, 150, 65, 100, 80, 90, 40, 80, 340]):
             self.tabelaEditovanje.setColumnWidth(i, width)
+        # Если hide True/верно, данный столбец будет скрыт; в противном случае это будет показано.
+        self.tabelaEditovanje.setColumnHidden(0, True)  # id не показываем!!!
 
     def prikazi_drugi(self):
         self.edit_window.show()
@@ -63,19 +89,70 @@ class Glavni_prozor(QWidget):
         self.edit_window.close()
         super(Glavni_prozor, self).close()
 
+    # --- +++
     def select_id(self, index):
-        id_read = index.row() + 1
-        print(id_read)
-        konekcija = sqlite_conect()
-        konekcija.sqlite_conect("", "")
-        konekcija.cursor.execute("SELECT * FROM telImenik WHERE id= '%s'" % (id_read))
-        row = konekcija.cursor.fetchone()
-        print(row)
+
+        # ---        id_read=index.row()+1
+        # ---        print(id_read)
+        # ---        konekcija = sqlite_conect()
+        # ---        konekcija.sqlite_conect("", "")
+        # ---        konekcija.cursor.execute("SELECT * FROM telImenik WHERE id= '%s'" % (id_read))
+        # ---        row = konekcija.cursor.fetchone()
+        # ---        print(row)
+
+        # +++
+        row = index.row()
+        self.id_db = self.model.record(row).value("id")
+        print("row->`{}`, id_db->`{}`".format(row, self.id_db))
+
+        self.edit_window.le_prezime.setText(self.model.record(row).value("Prezime"))
+        self.edit_window.le_ime.setText(self.model.record(row).value("Ime"))
+        self.edit_window.le_telefon.setText(self.model.record(row).value("Telefon"))
+        self.edit_window.le_lokal.setText(self.model.record(row).value("Lokal"))
+        self.edit_window.le_fax.setText(self.model.record(row).value("Fax"))
+        self.edit_window.le_oblast.setText(self.model.record(row).value("Oblast"))
+
+    # +++
+    def clickedBtnSave(self):
+        prezime = self.edit_window.le_prezime.text()
+        ime = self.edit_window.le_ime.text()
+        telefon = self.edit_window.le_telefon.text()
+        lokal = self.edit_window.le_lokal.text()
+        fax = self.edit_window.le_fax.text()
+        oblast = self.edit_window.le_oblast.text()
+
+        self.editDb(self.id_db, prezime, ime, telefon, lokal, fax, oblast)
+
+    # +++
+    def editDb(self, id_db, prezime, ime, telefon, lokal, fax, oblast):
+        self.edit_window.hide()
+        q = QtSql.QSqlQuery(""" UPDATE telImenik 
+                                   SET Prezime = '{}',
+                                       Ime     = '{}',
+                                       Telefon = '{}',
+                                       Lokal   = '{}',
+                                       Fax     = '{}',
+                                       Oblast  = '{}'
+                                 WHERE id =  '{}'
+                            """.format(prezime, ime, telefon, lokal, fax, oblast, id_db))
+
+        result = q.exec_()
+        if result:
+            self.model.query().exec_()
+            print("Ok!!!")
+        else:
+            print("Error ->", self.model.query().lastError().text())
+        return result
+
+    # +++
+    def clickedBtnCancel(self):
+        self.edit_window.hide()
 
 
 class edit_window(QWidget):
     def __init__(self, parent):
         super(edit_window, self).__init__()
+
         edit_window.setWindowTitle(self, 'Izmjena podataka postojećeg korisnika')
         self.setFixedSize(500, 400)
         self.layout = QVBoxLayout()
@@ -95,6 +172,7 @@ class edit_window(QWidget):
         self.label.setAlignment(QtCore.Qt.AlignCenter)
         self.label.setObjectName("label")
         self.label.setText('Izmjena postojećeg kontakta')
+
         self.le_prezime = QtWidgets.QLineEdit(self.groupBox)
         self.le_prezime.setGeometry(QtCore.QRect(160, 130, 150, 20))
         self.le_prezime.setObjectName("le_prezime")
@@ -143,14 +221,23 @@ class edit_window(QWidget):
         self.lbl_oblast.setAlignment(QtCore.Qt.AlignRight | QtCore.Qt.AlignTrailing | QtCore.Qt.AlignVCenter)
         self.lbl_oblast.setObjectName("lbl_oblast")
         self.lbl_oblast.setText('Oblast')
-        self.btn_save = QtWidgets.QPushButton(self.groupBox)
+
+        # ---    self.btn_save = QtWidgets.QPushButton(self.groupBox)
+        # +++
+        self.btn_save = QtWidgets.QPushButton(self.groupBox, clicked=parent.clickedBtnSave)
+
         self.btn_save.setGeometry(QtCore.QRect(140, 340, 80, 25))
         self.btn_save.setObjectName("btn_save")
         self.btn_save.setText('Snimi izmjene')
-        self.btn_cancel = QtWidgets.QPushButton(self.groupBox)
+
+        # ---    self.btn_cancel = QtWidgets.QPushButton(self.groupBox)
+        # +++
+        self.btn_cancel = QtWidgets.QPushButton(self.groupBox, clicked=parent.clickedBtnCancel)
+
         self.btn_cancel.setGeometry(QtCore.QRect(230, 340, 80, 25))
         self.btn_cancel.setObjectName("btn_cancel")
         self.btn_cancel.setText('Odustani')
+
         self.layout.addWidget(self.groupBox)
 
     def select_id(self, index):
